9535082c7613e4cd4d5dc850939f287c9a10b5db
Geoffrey Noël <nono@geoffreynoel.fr>
2015-08-14 02:54:02 +0200

    SocketIO

--- app/src/main/java/nonocopter/fr/nonocopter/Pilot.java
+++ app/src/main/java/nonocopter/fr/nonocopter/Pilot.java
@@ -1,58 +1,135 @@
 package nonocopter.fr.nonocopter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.hardware.SensorEventListener;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.VideoView;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.MalformedURLException;
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
 
 public class Pilot extends Activity {
 
     Navigator nav;
+    SocketIO socket;
+    ProgressDialog progressCalibrage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilot);
-        registerReceiver( onBroadcastReceived, new IntentFilter( "EVENT_DISCONNECTED"));
-        registerReceiver( onBroadcastReceived, new IntentFilter( "EVENT_CONNECTED"));
-        ImageButton btnEmergency = ( ImageButton) findViewById( R.id.btnEmergency);
-        ImageButton btnConnect   = ( ImageButton) findViewById( R.id.btnConnect);
-        ImageButton btnVideo     = ( ImageButton) findViewById( R.id.btnVideo);
-        ImageButton btnPhoto     = ( ImageButton) findViewById( R.id.btnPhoto);
-        ImageButton btnMoveUp    = ( ImageButton) findViewById( R.id.btnMoveUp);
-        ImageButton btnMoveDown  = ( ImageButton) findViewById( R.id.btnMoveDown);
-        ImageButton btnMoveLeft  = ( ImageButton) findViewById( R.id.btnMoveLeft);
-        ImageButton btnMoveRight = ( ImageButton) findViewById( R.id.btnMoveRight);
-        ImageButton btnNavigate  = ( ImageButton) findViewById( R.id.btnNavigate);
-        VideoView videoView      = ( VideoView)   findViewById( R.id.videoView);
-        setButtonColor( R.id.btnEmergency, Color.RED);
-        setButtonColor(R.id.btnConnect, Color.RED);
+        registerReceiver(onBroadcastReceived, new IntentFilter("EVENT_DISCONNECTED"));
+        registerReceiver(onBroadcastReceived, new IntentFilter("EVENT_CONNECTED"));
+        ImageButton btnEmergency = (ImageButton) findViewById(R.id.btnEmergency);
+        ImageButton btnConnect   = (ImageButton) findViewById(R.id.btnConnect);
+        ImageButton btnVideo     = (ImageButton) findViewById(R.id.btnVideo);
+        ImageButton btnPhoto     = (ImageButton) findViewById(R.id.btnPhoto);
+        ImageButton btnMoveUp    = (ImageButton) findViewById(R.id.btnMoveUp);
+        ImageButton btnMoveDown  = (ImageButton) findViewById(R.id.btnMoveDown);
+        ImageButton btnMoveLeft  = (ImageButton) findViewById(R.id.btnMoveLeft);
+        ImageButton btnMoveRight = (ImageButton) findViewById(R.id.btnMoveRight);
+        ImageButton btnNavigate  = (ImageButton) findViewById(R.id.btnNavigate);
+        ImageButton btnCalib     = (ImageButton) findViewById(R.id.btnCalib);
+        setButtonColor(R.id.btnEmergency, Color.RED);
+        setButtonColor(R.id.btnConnect,   Color.RED);
         btnEmergency.setOnClickListener(onClickListener);
-        btnConnect  .setOnClickListener(onClickListener);
-        btnVideo    .setOnClickListener(onClickListener);
-        btnPhoto    .setOnClickListener(onClickListener);
-        btnMoveUp   .setOnTouchListener(onTouchListener);
-        btnMoveDown .setOnTouchListener(onTouchListener);
-        btnMoveLeft .setOnTouchListener(onTouchListener);
+        btnConnect.setOnClickListener(onClickListener);
+        btnVideo.setOnClickListener(onClickListener);
+        btnPhoto.setOnClickListener(onClickListener);
+        btnMoveUp.setOnTouchListener(onTouchListener);
+        btnMoveDown.setOnTouchListener(onTouchListener);
+        btnMoveLeft.setOnTouchListener(onTouchListener);
         btnMoveRight.setOnTouchListener(onTouchListener);
-        btnNavigate .setOnTouchListener(onTouchListener);
-        videoView   .setOnTouchListener(onTouchListener);
-        if ( new ConnexionManager().isConnectedToCopter(this)) showConnected();
+        btnNavigate.setOnTouchListener(onTouchListener);
+        btnCalib.setOnClickListener(onClickListener);
+        if (new ConnexionManager().isConnectedToCopter(this)) showConnected();
+    }
+
+    private void toast( String message){
+        Toast.makeText(Pilot.this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void sendSocket( String event){
+        sendSocket(event, null, null);
+    }
+
+    private void sendSocket( String event, JSONException data){
+        sendSocket( event, data, null);
+    }
+
+    private void sendSocket( String event, IOAcknowledge ack){
+        sendSocket( event, null, ack);
+    }
+
+    private void sendSocket( String event, JSONException data, IOAcknowledge ack){
+        if ( socket == null ){
+            try {
+                socket = new SocketIO();
+                socket.connect("http://192.168.0.11:8080/", new IOCallback() {
+                    @Override
+                    public void onMessage(JSONObject json, IOAcknowledge ack) {
+                        try {
+                            System.out.println("################Server said Json:" + json.toString(2));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onMessage(String data, IOAcknowledge ack) {
+                        System.out.println("###################Server said String: " + data);
+                    }
+
+                    @Override
+                    public void onError(SocketIOException socketIOException) {
+                        System.out.println("###############an Error occured");
+                        if ( progressCalibrage != null ) progressCalibrage.dismiss();
+                        socketIOException.printStackTrace();
+                    }
+
+                    @Override
+                    public void onDisconnect() {
+                        System.out.println("#################Connection terminated.");
+                    }
+
+                    @Override
+                    public void onConnect() {
+
+                        Pilot.this.runOnUiThread( new Runnable(){
+                            @Override
+                            public void run() {
+                                //toast( "Yahou");
+                            }
+                        });
+                        System.out.println("##############Connection established");
+                    }
+
+                    @Override
+                    public void on(String event, IOAcknowledge ack, Object... args) {
+                        System.out.println("############Server triggered event '" + event + "'");
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace(); // Todo
+            }
+        }
+        if ( socket != null) socket.emit( event, ack, data);
     }
 
     private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
@@ -60,9 +137,6 @@ public class Pilot extends Activity {
         public boolean onTouch(View v, MotionEvent event) {
             int e = event.getAction();
             switch( v.getId()) {
-                case R.id.videoView:
-                    PlayVideo();
-                    break;
                 case R.id.btnMoveUp:
                     if ( e == MotionEvent.ACTION_DOWN || e == MotionEvent.ACTION_MOVE ) moveDrone( "up");
                     break;
@@ -96,6 +170,9 @@ public class Pilot extends Activity {
                 case R.id.btnPhoto:
                     takePhoto();
                     break;
+                case R.id.btnCalib:
+                    startCalibration();
+                    break;
             }
         }
     };
@@ -109,8 +186,8 @@ public class Pilot extends Activity {
         if ( e == MotionEvent.ACTION_UP || e == MotionEvent.ACTION_CANCEL ) nav.stop();
     }
 
-    public void moveDrone( String action){ // TODO
-        switch( action.toLowerCase()){
+    public void moveDrone( String action) { // TODO
+        switch (action.toLowerCase()) {
             case "up":
                 Toast.makeText(this, "BTN UP", Toast.LENGTH_LONG).show();
                 break;
@@ -126,26 +203,6 @@ public class Pilot extends Activity {
         }
     }
 
-    private void PlayVideo() { // TODO
-        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
-        try {
-            Uri video = Uri.parse( "http://192.168.0.11:8160" );
-            videoView.setVideoURI(video);
-            videoView.requestFocus();
-            Toast.makeText(this, getString(R.string.stream_loading), Toast.LENGTH_LONG).show();
-            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
-            {
-                public void onPrepared(MediaPlayer mp)
-                {
-                    videoView.start();
-                }
-            });
-        }
-        catch(Exception e) {
-            Toast.makeText(this, getString(R.string.stream_error), Toast.LENGTH_LONG).show();
-        }
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -153,7 +210,7 @@ public class Pilot extends Activity {
     }
 
     public void startConnexion() {
-        final Context c = this;
+        /*final Context c = this;
         final ProgressDialog progress = ProgressDialog.show( this, getString( R.string.loadT_connecting), getString( R.string.load_connecting), true, false);
         new AsyncTask<Void, Void, Boolean>() {
             @Override
@@ -167,7 +224,7 @@ public class Pilot extends Activity {
                 if ( connected ) showConnected();
                 else showDisconnected();
             }
-        }.execute(null, null, null);
+        }.execute(null, null, null);*/
     }
 
     BroadcastReceiver onBroadcastReceived = new BroadcastReceiver() {
@@ -181,7 +238,7 @@ public class Pilot extends Activity {
     };
 
     public void showConnected(){
-        setButtonColor( R.id.btnConnect, Color.GREEN);
+        setButtonColor( R.id.btnConnect, Color.YELLOW);
         Toast.makeText(this, getString(R.string.connected), Toast.LENGTH_LONG).show();
     }
 
@@ -197,6 +254,49 @@ public class Pilot extends Activity {
 
     public void takePhoto(){ // TODO
         Toast.makeText(this, getString(R.string.take_photo), Toast.LENGTH_LONG).show();
+    }
+
+    public void startCalibration(){
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if ( which == DialogInterface.BUTTON_POSITIVE ){
+                    progressCalibrage = ProgressDialog.show( Pilot.this, getString( R.string.calibrageT_load), getString( R.string.calibrage_load), true, false);
+                    //final ProgressDialog progress = ProgressDialog.show( Pilot.this, getString( R.string.calibrageT_load), getString( R.string.calibrage_load), true, false);
+                    IOAcknowledge callback = new IOAcknowledge() {
+                        @Override
+                        public void ack(final Object... objects) {
+                            Pilot.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressCalibrage.dismiss();
+                                    //progress.dismiss();
+                                    if ( objects.length <= 0 ) return;
+                                    switch ( (Integer) objects[0]) {
+                                        case 0:
+                                            toast( getString( R.string.calibrage_ko));
+                                            break;
+                                        case 1:
+                                            toast( getString( R.string.calibrage_ok));
+                                            break;
+                                        case 2:
+                                            toast( getString( R.string.calibrage_again));
+                                            break;
+                                        default:
+                                    }
+                                }
+                            });
+                        }
+                    };
+                    sendSocket( "startCalibration", callback);
+                }
+            }
+        };
+        AlertDialog.Builder yesNo = new AlertDialog.Builder( Pilot.this);
+        yesNo.setMessage( getString( R.string.startCalibrage));
+        yesNo.setPositiveButton(getString(R.string.yes), dialogClickListener);
+        yesNo.setNegativeButton(getString(R.string.no),  dialogClickListener);
+        yesNo.show();
     }
 
     public void setButtonColor( int btnId, int color) {
